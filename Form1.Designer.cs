0fcaa3b3e3af1bae00eaace0a3026365be479676
xmariia55x <mgm7cns@uma.es>
2020-12-06 15:43:56 +0100

    Añadidos dos botones y establecidas las medidas de los forms, tipo de letra, tamaño...

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,40 +29,46 @@ namespace Grupo02PCSAS
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button1.Font = new System.Drawing.Font("Trebuchet MS", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(978, 22);
+            this.button1.Location = new System.Drawing.Point(1289, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(159, 43);
+            this.button1.Size = new System.Drawing.Size(159, 49);
             this.button1.TabIndex = 0;
             this.button1.Text = "Iniciar sesión";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button2.Font = new System.Drawing.Font("Trebuchet MS", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(978, 92);
+            this.button2.Location = new System.Drawing.Point(1289, 86);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(159, 43);
+            this.button2.Size = new System.Drawing.Size(159, 49);
             this.button2.TabIndex = 1;
             this.button2.Text = "Regístrate";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1182, 1055);
+            this.ClientSize = new System.Drawing.Size(1469, 1033);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
