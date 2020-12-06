f694edd9dad1330911f6d11b6b90c3dfab37b16f
xmariia55x <mgm7cns@uma.es>
2020-12-06 14:17:33 +0100

    Commit de Maria

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace Grupo02PCSAS
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //Commit de maria
         }
     }
 }
