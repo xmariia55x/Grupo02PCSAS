164503f36d4bc90c97eb35450d5adee3665b92be
xmariia55x <mgm7cns@uma.es>
2020-12-08 10:23:53 +0100

    Creado formulario pantalla principal profesor

---
+++ fPrincipalProfesor.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Grupo02PCSAS
+{
+    partial class fPrincipalProfesor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // fPrincipalProfesor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(1469, 1033);
+            this.Name = "fPrincipalProfesor";
+            this.Text = "fPrincipalProfesor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
