9bab13095fce20dd738682fe7af0a8144d56bf47
Davidgc16 <davidgerecomi@uma.es>
2020-12-08 10:11:27 +0100

    commit delDavi

---
+++ fDatosPerfil.Designer.cs
@@ -0,0 +1,374 @@
+﻿
+namespace Grupo02PCSAS
+{
+	partial class fDatosPerfil
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fDatosPerfil));
+            this.bEditarPerfil = new System.Windows.Forms.Button();
+            this.bBorrarPerfil = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lNombreYApellidos = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lEmail = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lCIF = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lNIU = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lNombre = new System.Windows.Forms.Label();
+            this.lApellidos = new System.Windows.Forms.Label();
+            this.lRol = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bEditarPerfil
+            // 
+            this.bEditarPerfil.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bEditarPerfil.Font = new System.Drawing.Font("Trebuchet MS", 10.8F);
+            this.bEditarPerfil.Location = new System.Drawing.Point(217, 909);
+            this.bEditarPerfil.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bEditarPerfil.Name = "bEditarPerfil";
+            this.bEditarPerfil.Size = new System.Drawing.Size(204, 91);
+            this.bEditarPerfil.TabIndex = 2;
+            this.bEditarPerfil.Text = "Editar Perfil";
+            this.bEditarPerfil.UseVisualStyleBackColor = false;
+            // 
+            // bBorrarPerfil
+            // 
+            this.bBorrarPerfil.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bBorrarPerfil.Font = new System.Drawing.Font("Trebuchet MS", 10.8F);
+            this.bBorrarPerfil.Location = new System.Drawing.Point(519, 909);
+            this.bBorrarPerfil.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.bBorrarPerfil.Name = "bBorrarPerfil";
+            this.bBorrarPerfil.Size = new System.Drawing.Size(204, 91);
+            this.bBorrarPerfil.TabIndex = 3;
+            this.bBorrarPerfil.Text = "Borrar Perfil";
+            this.bBorrarPerfil.UseVisualStyleBackColor = false;
+            this.bBorrarPerfil.Click += new System.EventHandler(this.bBorrarPerfil_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(227, 29);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Nombre y Apellidos:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lNombreYApellidos
+            // 
+            this.lNombreYApellidos.AutoSize = true;
+            this.lNombreYApellidos.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lNombreYApellidos.Location = new System.Drawing.Point(250, 78);
+            this.lNombreYApellidos.Name = "lNombreYApellidos";
+            this.lNombreYApellidos.Size = new System.Drawing.Size(280, 29);
+            this.lNombreYApellidos.TabIndex = 12;
+            this.lNombreYApellidos.Text = "TU NOMBRE Y APELLIDOS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 29);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Email:";
+            // 
+            // lEmail
+            // 
+            this.lEmail.AutoSize = true;
+            this.lEmail.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lEmail.Location = new System.Drawing.Point(250, 118);
+            this.lEmail.Name = "lEmail";
+            this.lEmail.Size = new System.Drawing.Size(113, 29);
+            this.lEmail.TabIndex = 14;
+            this.lEmail.Text = "TU EMAIL";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(3, 154);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 29);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "CIF ONG:";
+            // 
+            // lCIF
+            // 
+            this.lCIF.AutoSize = true;
+            this.lCIF.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lCIF.Location = new System.Drawing.Point(250, 154);
+            this.lCIF.Name = "lCIF";
+            this.lCIF.Size = new System.Drawing.Size(84, 29);
+            this.lCIF.TabIndex = 16;
+            this.lCIF.Text = "TU CIF";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 192);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 29);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "NIU:";
+            // 
+            // lNIU
+            // 
+            this.lNIU.AutoSize = true;
+            this.lNIU.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lNIU.Location = new System.Drawing.Point(250, 192);
+            this.lNIU.Name = "lNIU";
+            this.lNIU.Size = new System.Drawing.Size(88, 29);
+            this.lNIU.TabIndex = 18;
+            this.lNIU.Text = "TU NIU";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(3, 231);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(143, 29);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Contraseña:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(250, 231);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(76, 29);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "*******";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Trebuchet MS", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(871, 532);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(395, 29);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Mis Actividades y cursos realizados:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(876, 610);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(378, 222);
+            this.dataGridView1.TabIndex = 22;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lNombre);
+            this.panel1.Controls.Add(this.lApellidos);
+            this.panel1.Controls.Add(this.lRol);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1477, 415);
+            this.panel1.TabIndex = 43;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Beige;
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.ImageLocation = "C:\\Users\\maria\\OneDrive - Universidad de Málaga\\UNIVERSIDAD\\Tercer año\\1er cuatri" +
+    "mestre\\Ingeniería de requisitos\\Proyecto";
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 258);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(653, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 49);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Hola,";
+            // 
+            // lNombre
+            // 
+            this.lNombre.AutoSize = true;
+            this.lNombre.Font = new System.Drawing.Font("Trebuchet MS", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lNombre.Location = new System.Drawing.Point(771, 100);
+            this.lNombre.Name = "lNombre";
+            this.lNombre.Size = new System.Drawing.Size(163, 49);
+            this.lNombre.TabIndex = 13;
+            this.lNombre.Text = "NOMBRE";
+            // 
+            // lApellidos
+            // 
+            this.lApellidos.AutoSize = true;
+            this.lApellidos.Font = new System.Drawing.Font("Trebuchet MS", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lApellidos.Location = new System.Drawing.Point(940, 100);
+            this.lApellidos.Name = "lApellidos";
+            this.lApellidos.Size = new System.Drawing.Size(201, 49);
+            this.lApellidos.TabIndex = 14;
+            this.lApellidos.Text = "APELLIDOS";
+            // 
+            // lRol
+            // 
+            this.lRol.AutoSize = true;
+            this.lRol.Font = new System.Drawing.Font("Trebuchet MS", 10.2F);
+            this.lRol.Location = new System.Drawing.Point(1088, 336);
+            this.lRol.Name = "lRol";
+            this.lRol.Size = new System.Drawing.Size(66, 23);
+            this.lRol.TabIndex = 16;
+            this.lRol.Text = "TU ROL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 10.2F);
+            this.label2.Location = new System.Drawing.Point(1043, 336);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 23);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Rol:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 10.2F);
+            this.label4.Location = new System.Drawing.Point(1259, 336);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 23);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Mi perfil";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.lNombreYApellidos);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.lEmail);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.lCIF);
+            this.panel2.Controls.Add(this.lNIU);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Location = new System.Drawing.Point(217, 482);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(535, 374);
+            this.panel2.TabIndex = 44;
+            // 
+            // fDatosPerfil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Beige;
+            this.ClientSize = new System.Drawing.Size(1469, 1033);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.bBorrarPerfil);
+            this.Controls.Add(this.bEditarPerfil);
+            this.Font = new System.Drawing.Font("Trebuchet MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "fDatosPerfil";
+            this.Text = "fDatosPerfil";
+            this.Load += new System.EventHandler(this.fDatosPerfil_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.fDatosPerfil_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button bEditarPerfil;
+		private System.Windows.Forms.Button bBorrarPerfil;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label lNombreYApellidos;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label lEmail;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label lCIF;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label lNIU;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lNombre;
+        private System.Windows.Forms.Label lApellidos;
+        private System.Windows.Forms.Label lRol;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
