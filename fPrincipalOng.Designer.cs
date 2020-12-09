f6c6f8818dcdf443cb17f23bc17830340847ec3e
YerayRuiz <yeray.r7@uma.es>
2020-12-09 09:07:19 +0100

    commit de yeray

---
+++ fPrincipalOng.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Grupo02PCSAS
+{
+    partial class fPrincipalOng
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fPrincipalOng));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lRol = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lNombreUser = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bCrearActividad = new System.Windows.Forms.Button();
+            this.bAccederAlForo = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bInformación = new System.Windows.Forms.Button();
+            this.bModificar = new System.Windows.Forms.Button();
+            this.bBorrar = new System.Windows.Forms.Button();
+            this.bParticipantes = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Beige;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.lRol);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lNombreUser);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1469, 177);
+            this.panel1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(1283, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 26);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Mi perfil";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1277, 11);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 103);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // lRol
+            // 
+            this.lRol.AutoSize = true;
+            this.lRol.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lRol.Location = new System.Drawing.Point(551, 116);
+            this.lRol.Name = "lRol";
+            this.lRol.Size = new System.Drawing.Size(63, 26);
+            this.lRol.TabIndex = 5;
+            this.lRol.Text = "Tu rol";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(507, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Rol: ";
+            // 
+            // lNombreUser
+            // 
+            this.lNombreUser.AutoSize = true;
+            this.lNombreUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lNombreUser.Location = new System.Drawing.Point(621, 39);
+            this.lNombreUser.Name = "lNombreUser";
+            this.lNombreUser.Size = new System.Drawing.Size(346, 44);
+            this.lNombreUser.TabIndex = 3;
+            this.lNombreUser.Text = "Nombre y apellidos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(503, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 49);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hola,";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(297, 178);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bCrearActividad
+            // 
+            this.bCrearActividad.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bCrearActividad.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCrearActividad.Location = new System.Drawing.Point(973, 771);
+            this.bCrearActividad.Name = "bCrearActividad";
+            this.bCrearActividad.Size = new System.Drawing.Size(181, 63);
+            this.bCrearActividad.TabIndex = 55;
+            this.bCrearActividad.Text = "Crear actividad";
+            this.bCrearActividad.UseVisualStyleBackColor = false;
+            this.bCrearActividad.Click += new System.EventHandler(this.bCrearCurso_Click);
+            // 
+            // bAccederAlForo
+            // 
+            this.bAccederAlForo.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bAccederAlForo.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bAccederAlForo.Location = new System.Drawing.Point(230, 771);
+            this.bAccederAlForo.Name = "bAccederAlForo";
+            this.bAccederAlForo.Size = new System.Drawing.Size(181, 63);
+            this.bAccederAlForo.TabIndex = 54;
+            this.bAccederAlForo.Text = "Acceder al foro";
+            this.bAccederAlForo.UseVisualStyleBackColor = false;
+            this.bAccederAlForo.Click += new System.EventHandler(this.bAccederAlForo_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(116, 371);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1113, 350);
+            this.dataGridView1.TabIndex = 53;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(640, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(279, 49);
+            this.label3.TabIndex = 52;
+            this.label3.Text = "Mis actividades";
+            // 
+            // bInformación
+            // 
+            this.bInformación.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bInformación.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bInformación.Location = new System.Drawing.Point(1235, 465);
+            this.bInformación.Name = "bInformación";
+            this.bInformación.Size = new System.Drawing.Size(181, 63);
+            this.bInformación.TabIndex = 56;
+            this.bInformación.Text = "Información";
+            this.bInformación.UseVisualStyleBackColor = false;
+            // 
+            // bModificar
+            // 
+            this.bModificar.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bModificar.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bModificar.Location = new System.Drawing.Point(1235, 561);
+            this.bModificar.Name = "bModificar";
+            this.bModificar.Size = new System.Drawing.Size(181, 63);
+            this.bModificar.TabIndex = 57;
+            this.bModificar.Text = "Modificar";
+            this.bModificar.UseVisualStyleBackColor = false;
+            // 
+            // bBorrar
+            // 
+            this.bBorrar.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bBorrar.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBorrar.Location = new System.Drawing.Point(1235, 658);
+            this.bBorrar.Name = "bBorrar";
+            this.bBorrar.Size = new System.Drawing.Size(181, 63);
+            this.bBorrar.TabIndex = 58;
+            this.bBorrar.Text = "Borrar";
+            this.bBorrar.UseVisualStyleBackColor = false;
+            // 
+            // bParticipantes
+            // 
+            this.bParticipantes.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.bParticipantes.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bParticipantes.Location = new System.Drawing.Point(1235, 371);
+            this.bParticipantes.Name = "bParticipantes";
+            this.bParticipantes.Size = new System.Drawing.Size(181, 63);
+            this.bParticipantes.TabIndex = 59;
+            this.bParticipantes.Text = "Ver participantes";
+            this.bParticipantes.UseVisualStyleBackColor = false;
+            // 
+            // fPrincipalOng
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Beige;
+            this.ClientSize = new System.Drawing.Size(1469, 1033);
+            this.Controls.Add(this.bParticipantes);
+            this.Controls.Add(this.bBorrar);
+            this.Controls.Add(this.bModificar);
+            this.Controls.Add(this.bInformación);
+            this.Controls.Add(this.bCrearActividad);
+            this.Controls.Add(this.bAccederAlForo);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Name = "fPrincipalOng";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "fPrincipalOng";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lRol;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lNombreUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button bCrearActividad;
+        private System.Windows.Forms.Button bAccederAlForo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button bInformación;
+        private System.Windows.Forms.Button bModificar;
+        private System.Windows.Forms.Button bBorrar;
+        private System.Windows.Forms.Button bParticipantes;
+    }
+}
