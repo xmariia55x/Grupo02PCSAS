238957e7f6c6c27e7a56a95ceb6fbd40491c4a4b
xmariia55x <mgm7cns@uma.es>
2021-01-02 12:55:35 +0100

    Probando el de principal profesor

--- fPrincipalProfesor.Designer.cs
+++ fPrincipalProfesor.Designer.cs
@@ -421,8 +421,8 @@ namespace Grupo02PCSAS
             // 
             // fPrincipalProfesor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FloralWhite;
