83f6695df540e0c6dd79de5c9f70fbbae0567eed
AdriCorrales <adricorrales12345@gmail.com>
2021-01-05 14:10:22 +0100

    Noticias.cs terminado

---
+++ MaterialActividad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo02PCSAS
+{
+    class MaterialActividad
+    {
+        private static string BD_SERVER = Properties.Settings.Default.BD_SERVER;
+        private static string BD_NAME = Properties.Settings.Default.BD_NAME;
+
+        private Actividad actividad;
+        private string enlace;
+        private string nombre;
+
+        public MaterialActividad(int idA, String n)
+        {
+            SQLSERVERDB miBD = new SQLSERVERDB(BD_SERVER, BD_NAME);
+
+        }
+    }
+}
