39d88eb2f82bd98aee55dd0faa5d6dbc03f9e8b4
xmariia55x <mgm7cns@uma.es>
2021-01-12 18:37:12 +0100

    El program esta tal y como estaba

--- Program.cs
+++ Program.cs
@@ -92,7 +92,7 @@ namespace Grupo02PCSAS
             main.FormClosed += MainForm_Closed;
             main.Show();
             Application.Run();
-            //Application.Run(new fPruebaConocimientoCurso(new Usuario("mgm7cns@uma.es"), new Curso(24)));
+           
 
             //Correo.sendEmail(Correo.recordatorioEvento("12/1/2021"), "Recordatorio", new Usuario("mariagalvez12345@gmail.com"));
             //Curso c = new Curso(24);
