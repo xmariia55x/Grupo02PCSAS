ab00c077874667ef23950948af76d093b46356d2
YerayRuiz <yeray.r7@uma.es>
2021-01-14 09:21:55 +0100

    fEliminarArchivos funcionando correctly

--- fEliminarArchivoActividad.Designer.cs
+++ fEliminarArchivoActividad.Designer.cs
@@ -188,6 +188,7 @@ namespace Grupo02PCSAS
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 24;
             this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
             // 
             // dataGridView1
             // 
